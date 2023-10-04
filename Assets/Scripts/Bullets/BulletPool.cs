02f846c6bed963a808ee3fc1c1119f5271b05790
RagulPrasadG <gulshark572@gmail.com>
2023-10-04 16:17:56 +0530

    Implemented GetBullet() in BulletPool

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -13,7 +13,29 @@ namespace CosmicCuration.Bullets
         public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
         {
             this.bulletView = bulletView;
-            this.bulletScriptableObject = bulletScriptableObject;   
+            this.bulletScriptableObject = bulletScriptableObject;
+            pooledbullets = new List<PooledBullet>();
+        }
+        public BulletController GetBullet()
+        {
+            if(pooledbullets.Count > 0 )
+            {
+                PooledBullet pooledBullet = pooledbullets.Find(pooledBullet => !pooledBullet.isUsed);
+                if(pooledBullet != null)
+                {
+                    pooledBullet.isUsed = true;
+                    return pooledBullet.bulletController;
+                }
+            }
+            return CreatePooledBullet();
+        }
+
+        public BulletController CreatePooledBullet()
+        {
+            PooledBullet pooledBullet = new PooledBullet();
+            pooledBullet.bulletController = new BulletController(bulletView, bulletScriptableObject);
+            pooledBullet.isUsed = true;
+            return pooledBullet.bulletController;
         }
 
         public class PooledBullet
