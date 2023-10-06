8fb04880c941f2b6947e83922f325a53f8186b01
RagulPrasadG <gulshark572@gmail.com>
2023-10-06 14:36:08 +0530

    Create Generic GetItem method

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericObjectPool<T> where T : class
+{
+    private List<PooledObject<T>> pooledItems= new List<PooledObject<T>>();
+
+    protected T GetObject()
+    {
+        if (pooledItems.Count > 0)
+        {
+            PooledObject<T> item = pooledItems.Find(obj => !obj.isUsed);
+            if (item != null)
+            {
+                item.isUsed = true;
+                return item.Item;
+            }
+        }
+        return CreatePooledItem();
+
+    }
+
+    private T CreatePooledItem()
+    {
+        PooledObject<T> pooledItem = new PooledObject<T>();
+        pooledItem.Item = CreateItem();
+        pooledItem.isUsed = true;
+        pooledItems.Add(pooledItem);
+        return pooledItem.Item;
+    }
+
+    protected virtual T CreateItem()
+    {
+        throw new NotImplementedException("Child class must implement CreateItem() method");
+    }
+
+    public class PooledObject<T>
+        {
+             public T Item;
+             public bool isUsed;
+        }
+
+
+}
