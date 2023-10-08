9a1a213d71851aab295789fbf8a6024aa75697d9
RagulPrasadG <gulshark572@gmail.com>
2023-10-08 11:53:09 +0530

    Created PowerUp Pool Class

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CosmicCuration.Utilities;
+using System;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+        public PowerUpController GetPowerUp<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            if(typeof(T) == typeof(Shield))
+            {
+                return new Shield(this.powerUpData);
+            }
+            else if (typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(this.powerUpData);
+            }
+            else if (typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(this.powerUpData);
+            }
+            else
+            {
+                throw new NotSupportedException("Invalid powerup type");
+            }
+
+        }
+
+    }
+
+}
+
